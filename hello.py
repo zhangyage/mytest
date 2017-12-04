73ce101a6fca3154e8a2a8301a5d6812ef624f58
zhangyage <zhangyage2015@163.com>
2017-12-04 15:21:53 +0800

    使用flask-moment处理时间

--- hello.py
+++ hello.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+'''
+flask-moment  学习使用moment同一时间，  对应的直接使用本地电脑的时间，不在从服务器中获取时间
+'''
+
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/user/<name>/')
-def user(name):
-    return render_template('user.html',user=name)
+@app.route('/time/')
+def time():
+    return render_template('moment.html',current_time = datetime.utcnow())
 
 @app.route('/user/')
 def users():
