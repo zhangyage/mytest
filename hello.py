da451e10b0d5151b9da0e08ba6ce35b7afae0587
zhangyage <zhangyage2015@163.com>
2017-12-04 14:48:35 +0800

    flask使用jinjia2模板渲染网页

--- hello.py
+++ hello.py
@@ -19,6 +19,14 @@ def user(name):
 def users():
     return render_template('user.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
+
 
 if __name__ == "__main__":
     app.run(debug=True,port=int("80"))
