d2f2923268439fc807aa63ffd2f799311f28c91e
zhangyage <zhangyage2015@163.com>
2017-12-06 11:29:09 +0800

    使用flask扩展flask-migrate定制迁移脚本，更新我们的数据库结构

--- hello.py
+++ hello.py
@@ -1,32 +1,29 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-'''
-    flask_sqlalchemy  (ORM)
-'''
-
-
-from flask import Flask,render_template,session,url_for,redirect,flash
+import os
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import Required
-from datetime import datetime
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
-import os
-from flask_script.commands import Shell
+from flask_script import Manager,Shell
+from flask_migrate import Migrate, MigrateCommand
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///' +os.path.join(basedir, 'data.sqlite')   #设置数据库uri
-app.config[' SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True   #设置参数每次更改数据都需要提交
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-bootstarp = Bootstrap(app)
+
+bootstrap = Bootstrap(app)
 moment = Moment(app)
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand) 
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -42,16 +39,32 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    password = db.Column(db.String(64))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return '<User %r>' % self.username
 
 
-
 class NameForm(FlaskForm):
-    name = StringField('What is your name?',validators=[Required()])
+    name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -67,23 +80,9 @@ def index():
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'),known=session.get('known', False))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-# manager = Manager(app)    #结合shell注册上下文
-# def make_shell_context():
-#     return dict(app=app,db=db,User=User,Role=Role)
-
-
-if __name__ == "__main__":
-    app.run(debug=True,port=int("80"))
-    #manager.run()    #使用这个方式启动，是为了在shel模式下简历我们的数据库表
-    #manager.add_command("shell",Shell(make_context=make_shell_context()))
+if __name__=='__main__':  
+    manager.run()  
+    #app.run(port=int(80))
