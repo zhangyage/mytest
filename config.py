a434ce093c66edfcb9bc41361cb6582721925039
zhangyage <zhangyage2015@163.com>
2017-12-06 17:20:34 +0800

    ..

--- config.py
+++ config.py
@@ -1,2 +1,45 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'   #csrf密钥
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True                                  #设置sql自动提交
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'zhangyage2015@163.com'  #获取收件人
+    
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    
+    
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    
+    
+config = {
+        'development': DevelopmentConfig,
+        'testing': TestingConfig,
+        'production': ProductionConfig,
+        
+        'default': DevelopmentConfig
+}
