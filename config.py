aa9679cad09caa658b82f8e119d2b5583ef41073
zhangyage <zhangyage2015@163.com>
2017-12-07 17:10:54 +0800

    参数配置文件

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'   #csrf密钥
-    SQLALCHEMY_COMMIT_ON_TEARDOWN = True                                  #设置sql自动提交
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True    
+    SQLALCHEMY_TRACK_MODIFICATIONS = True                              #设置sql自动提交
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'zhangyage2015@163.com'  #获取收件人
