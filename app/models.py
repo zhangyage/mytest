0acb23614449a9ac48a67bffd0a941942aeb0633
zhangyage <zhangyage2015@163.com>
2017-12-11 15:22:25 +0800

    添加安全令牌确认模块

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from app import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -22,6 +24,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(255))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean,default=False)
 
     @property
     def password(self):
@@ -33,6 +36,22 @@ class User(UserMixin, db.Model):
         
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def generate_confirmation_token(self,expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'],expiration)
+        return s.dumps({'confirm':self.id})
+    
+    def confirm(self,token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.load(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
        
     def __repr__(self):
         return '<User %r>' % self.usernam
