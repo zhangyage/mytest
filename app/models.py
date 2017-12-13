af2e6f5cc38742c117cf6336f7b67dfc697a2fc6
zhangyage <zhangyage2015@163.com>
2017-12-13 16:52:48 +0800

    数据库模块

--- app/models.py
+++ app/models.py
@@ -6,13 +6,73 @@ from flask_login import UserMixin
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+from flask_login import UserMixin,AnonymousUserMixin
+from datetime import datetime
+
+
+
+class Permission:
+    FOLLOW = 1          #关注用户
+    COMMENT = 2         #评论他人的文章
+    WRITE = 4           #写文章
+    MODERATE = 8        #管理他人发表的评论  
+    ADMIN = 16          #管理员权限
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean,default=False,index=True)
+    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+   
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+    
+    @staticmethod
+    def insert_roles():
+        '''
+        @pram
+        'User'  普通用户  具有发布文章、发表评论和关注其他用户的权限。这是新用户的默认角色
+        'Moderator'  协调员  增加审查不当评论的权限
+        'Administrator' 管理员  具有所有权限，包括修改其他用户所属角色的权限
+        '''
+        roles = {
+            'User': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE],
+            'Moderator': [Permission.FOLLOW, Permission.COMMENT,
+                          Permission.WRITE, Permission.MODERATE],
+            'Administrator': [Permission.FOLLOW, Permission.COMMENT,
+                              Permission.WRITE, Permission.MODERATE,
+                              Permission.ADMIN],
+        }
+        default_role = 'User'
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.reset_permissions()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
 
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permissions(self):
+        self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+    
     def __repr__(self):
         return '<Role %r>' % self.name
 
@@ -25,7 +85,14 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(255))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean,default=False)
-
+    name = db.Column(db.String(64)) #名字
+    location = db.Column(db.String(64)) #所在地
+    about_me = db.Column(db.Text())  #关于我
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)  #注册日期
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)    #最后访问日期
+    
+    
+    
     @property
     def password(self):
         raise AttributeError('password is not a readble attibute')
@@ -52,9 +119,22 @@ class User(UserMixin, db.Model):
         self.confirmed = True
         db.session.add(self)
         return True
+    
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
        
     def __repr__(self):
         return '<User %r>' % self.usernam
+    
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+login_manager.anonymous_user = AnonymousUser
     
 @login_manager.user_loader
 def load_user(user_id):
