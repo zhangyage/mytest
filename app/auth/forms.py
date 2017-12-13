4018e0ca8bde477e797afa563daf49f0849bf1cc
zhangyage <zhangyage2015@163.com>
2017-12-13 16:53:25 +0800

    表单

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,7 +4,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,BooleanField,PasswordField
 from wtforms.validators import Required,Length,Email, regexp, EqualTo,\
-    ValidationError
+    ValidationError, DataRequired
 from app.models import User
 
 
@@ -63,4 +63,36 @@ class RegistrationForm(FlaskForm):
     
     def validate_username(self,field):
         if User.query.filter_by(username=field.data).first():
-            raise ValidationError('Username already in use')    
+            raise ValidationError('Username already in use')  
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old password', validators=[DataRequired()])
+    password = PasswordField('New password', validators=[
+        DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm new password',
+                              validators=[DataRequired()])
+    submit = SubmitField('Update Password')
+
+
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    submit = SubmitField('Reset Password')
+
+
+class PasswordResetForm(FlaskForm):
+    password = PasswordField('New Password', validators=[
+        DataRequired(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Reset Password')
+
+
+class ChangeEmailForm(FlaskForm):
+    email = StringField('New Email', validators=[DataRequired(), Length(1, 64),
+                                                 Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Update Email Address')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')  
