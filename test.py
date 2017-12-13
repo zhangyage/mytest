b9df57cb4126f6d9fbd8563df43459c17f0bf57e
zhangyage <zhangyage2015@163.com>
2017-12-13 16:55:07 +0800

    ..

---
+++ test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+roles = {
+            'User': [1, 
+                     2, 
+                     3],
+            'Moderator': [1, 
+                          2,
+                          3, 
+                          4],
+            'Administrator': [1, 
+                              2,
+                              3, 
+                              4,
+                              5],
+        }
+        
+        
+for r in roles:
+    print r
