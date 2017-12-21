daf25994182a451e2b1ebe5c61fb98690e557261
zhangyage <zhangyage2015@163.com>
2017-12-21 16:33:45 +0800

    表单处理/用户资料编辑

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField,SubmitField,BooleanField,PasswordField,TextAreaField
+from wtforms.validators import Required,Length,Email, regexp, EqualTo,\
+    ValidationError, DataRequired
+from app.models import User
+
 
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+    
+#用户资料编辑表单        
+class EditProfileForm(FlaskForm):
+     name = StringField('Real name', validators=[Length(0, 64)])
+     location = StringField('Location', validators=[Length(0, 64)])
+     about_me = TextAreaField('About me')
+     submit = SubmitField('Submit')
