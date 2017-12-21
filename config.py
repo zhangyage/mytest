40a5d72bd8414add27c3c582436f094586be3890
zhangyage <zhangyage2015@163.com>
2017-12-21 11:29:39 +0800

    配置文件

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True    
     SQLALCHEMY_TRACK_MODIFICATIONS = True                              #设置sql自动提交
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'zhangyage2015@163.com'
+    FLASKY_MAIL_SENDER = 'zhangyage2015@163.com'   #发件人
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'zhangyage2015@163.com'  #获取收件人
     
     @staticmethod
