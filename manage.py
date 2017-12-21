26451079d392a1b08f86e9e194eace762782dbf6
zhangyage <zhangyage2015@163.com>
2017-12-21 15:22:59 +0800

    程序启动入口，在程序上下文中添加了permssion

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import os
 from flask_migrate import Migrate,MigrateCommand
 from flask_script import Manager,Shell
 from app import create_app, db
-from app.models import User, Role
+from app.models import User, Role, Permission
 from sqlalchemy.pool import manage
 from test.test_basics import BasicsTestCase 
 
@@ -16,7 +16,7 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
