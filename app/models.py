2cbbca2cd4c02efecde88d22b071781bc0f2d887
zhangyage <zhangyage2015@163.com>
2017-12-22 11:38:05 +0800

    数据库模型  添加了图片缓存字段同时添加了图片处理模块

--- app/models.py
+++ app/models.py
@@ -5,9 +5,10 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app,request
 from flask_login import UserMixin,AnonymousUserMixin
 from datetime import datetime
+import hashlib
 
 
 
@@ -87,6 +88,7 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean,default=False)
     name = db.Column(db.String(64)) #名字
+    avatar_hash = db.Column(db.String(32))   #图片的临时缓存hash值
     location = db.Column(db.String(64)) #所在地
     about_me = db.Column(db.Text())  #关于我
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)  #注册日期
@@ -167,6 +169,7 @@ class User(UserMixin, db.Model):
         if self.query.filter_by(email=new_email).first() is not None:
             return False
         self.email = new_email
+        self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
         db.session.add(self)
         return True
 
@@ -176,10 +179,26 @@ class User(UserMixin, db.Model):
     def is_administrator(self):
         return self.can(Permission.ADMIN)
     
+    #用户操作记录，每次操作都会调用一下这个函数
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
-       
+        
+    #图像生成函数    
+    def gravatar(self,size=100,default='identicon',rating='g'):
+        '''
+        @param
+         size:图片大小  单位：像素
+         default:针对没有设置图片的设置默认值
+         rating:图片级别
+        '''
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url, hash=hash, size=size, default=default, rating=rating)
+    
     def __repr__(self):
         return '<User %r>' % self.usernam
     
