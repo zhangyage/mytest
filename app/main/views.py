ef2b2bd0238afc143c81b9ddc72b76fb579a898b
zhangyage <zhangyage2015@163.com>
2017-12-22 16:23:19 +0800

    视图路由

--- app/main/views.py
+++ app/main/views.py
@@ -4,10 +4,10 @@
 
 from flask import render_template,url_for,redirect,session,current_app,abort
 from app import db
-from app.models import User,Role
+from app.models import User,Role,Post,Permission
 from app.email import send_email
 from . import main
-from .forms import NameForm,EditProfileForm,EditProfileAdminForm
+from .forms import NameForm,EditProfileForm,EditProfileAdminForm,PostForm
 
 
 #导入装饰器
@@ -20,24 +20,34 @@ from flask.helpers import flash
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-                           'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),known=session.get('known', False))
-    
+    form = PostForm()
+    status = current_user.can(Permission.WRITE)
+    if current_user.can(Permission.WRITE) and form.validate_on_submit():
+        post = Post(body=form.body.data,
+                    author=current_user._get_current_object()
+                    )
+        db.session.add(post)
+        return redirect(url_for('main.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html',form=form,posts=posts,status=status)
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.name.data).first()
+#         if user is None:
+#             user = User(username=form.name.data)
+#             db.session.add(user)
+#             db.session.commit()
+#             session['known'] = False
+#             if current_app.config['FLASKY_ADMIN']:
+#                 send_email(current_app.config['FLASKY_ADMIN'], 'New User',
+#                            'mail/new_user', user=user)
+#         else:
+#             session['known'] = True
+#         session['name'] = form.name.data
+#         return redirect(url_for('.index'))
+#     return render_template('index.html',
+#                            form=form, name=session.get('name'),known=session.get('known', False))
+#     
 
 #用户资料展示页面    
 @main.route('/user/<username>')
@@ -45,7 +55,8 @@ def user(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
-    return render_template('user.html',user=user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html',user=user,posts=posts)
 
 #用户资料编辑页面
 @main.route('/edit-profile', methods=['GET', 'POST'])
@@ -90,6 +101,7 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
+
 
 
     
