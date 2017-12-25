c1c267a923d991b2dfabcd3ca087c63cc8bd8fd9
zhangyage <zhangyage2015@163.com>
2017-12-25 12:19:40 +0800

    配置文件/添加了分页页数

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'zhangyage2015@163.com'   #发件人
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'zhangyage2015@163.com'  #获取收件人
-    
+    FLASKY_POSTS_PER_PAGE = 20  #制定分页的页数
     @staticmethod
     def init_app(app):
         pass
