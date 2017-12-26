dd6a9df6ee421cd02e6fa8415e42d2c5cd8e8aac
zhangyage <zhangyage2015@163.com>
2017-12-26 14:58:12 +0800

    路由视图

--- app/main/views.py
+++ app/main/views.py
@@ -110,6 +110,14 @@ def edit_profile_admin(id):
     return render_template('edit_profile.html', form=form, user=user)
 
 
+#文章单页显示
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html',posts=[post])
+
+
+
 
     
 #装饰器函数测试用例
