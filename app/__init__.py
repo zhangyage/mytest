6ed145f36b35a963e04da66c0119e13ba471872a
zhangyage <zhangyage2015@163.com>
2017-12-26 10:37:18 +0800

    添加了pagedown模块，初始化对应的模块

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@
 
 from flask import Flask,render_template,url_for,redirect
 from flask_bootstrap import Bootstrap
+from flask_pagedown import PageDown
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -18,6 +19,8 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+pagedown = PageDown()
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])    #继承config.py中定义的
@@ -28,6 +31,8 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    
+    pagedown.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
