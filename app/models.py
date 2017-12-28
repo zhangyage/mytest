7dbaea708651561da1e3b7d739322301ba6441c3
zhangyage <zhangyage2015@163.com>
2017-12-28 17:21:32 +0800

    数据库模块，添加了对关注用户文章的提取模块

--- app/models.py
+++ app/models.py
@@ -258,8 +258,16 @@ class User(UserMixin, db.Model):
     def is_followed_by(self, user):
         return self.followers.filter_by(follower_id=user.id).first() is not None
     
+    
+    #连表查询，获取关注用户的文章   集合view中的
+    @property
+    def followed_posts(self):
+        return Post.query.join(Follow, Follow.followed_id == Post.author_id).filter(Follow.follower_id == self.id)
+    
+    
     def __repr__(self):
         return '<User %r>' % self.usernam
+    
     
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
