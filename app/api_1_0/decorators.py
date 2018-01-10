6bddf3273d62cceea444c506785e61d253f4b1b1
zhangyage <zhangyage2015@163.com>
2018-01-10 15:12:50 +0800

    14a-权限认证

--- app/api_1_0/decorators.py
+++ app/api_1_0/decorators.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
+from functools import wraps
+from flask import g
+from .errors import forbidden
+
+def permission_require(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not g.current_user.can(permission):
+                return forbidden('Insufficient permission')
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+        
